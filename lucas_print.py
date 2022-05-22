2c93ea00b49e19ebc05d3c1c6e8de45247319999
Lucas Matthew Lopez <lucasmlopez12@outlook.com>
2022-05-22 15:56:22 -0400

    Resolved Issue #2

---
+++ lucas_print.py
@@ -0,0 +1,19 @@
+print('1. This is my first sentence.')
+
+print('2. This is my second sentence.')
+
+print('3. I love my family, printing is good!')
+
+print('4. I am having a really good day!')
+
+print('5. G-day, mate?')
+
+print('6. Hey! I know what 3x6 is!')
+
+print('7. Hey dad! Can you print this out for me?')
+
+print('8. Hmm, I wonder how Jean is doing in Puerto Rico?')
+
+print('9. Lets go to the pool, it is such a great day outside!')
+
+print('10. Rest in peace for the people of Ukraine. Putin is not a good man.')
