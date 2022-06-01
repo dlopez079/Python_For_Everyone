3bcd49e09106a5b6ed275df3754e486244adddee
David Lopez <dlopez@mattmen.com>
2022-06-01 12:25:32 -0400

    Excercise Structure. I removed the README.md file.  I choose to add the instructions right into the program as a multiple line comment.

---
+++ ex_05_01/dl_05_01.py
@@ -0,0 +1,16 @@
+"""
+Exercise 1
+
+Write a program which repeatedly reads numbers until the user enters "done".  Once "done" is entered, print out the total, count, and average of the numbers.  If the user enters anything other than a number, detect their mistake using 'try' and 'except' and print an error message and skip to the next number.
+
+Enter a number: 4
+Enter a number: 5
+Enter a number: bad data
+Invalid input
+Enter a number: 7
+Enter a number: done
+16  3  5.3333333333
+"""
+
+
+
