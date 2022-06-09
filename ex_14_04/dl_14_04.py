3e976d76078228b611b2c7ae7c5ee57096e1f2c7
David Lopez <dlopez@mattmen.com>
2022-06-09 12:16:48 -0400

    Chapter 14.4 - Object In heritance / Def

---
+++ ex_14_04/dl_14_04.py
@@ -0,0 +1,11 @@
+"""
+Definitions
+
+Class > a template
+Attribute > A variable within a class
+Method > A function within a class
+Object > A particular instance of a class
+Contructor > Code that runs when an object is created
+Inheritence - The ability to extend a class to make a new class.
+
+"""
