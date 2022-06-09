e542e4665e32a8fca12b8c1fd12073a84b5f3852
David Lopez <dlopez@mattmen.com>
2022-06-09 12:14:47 -0400

    Chapter 14.1 - Object Oriented Definitions and Terminology

---
+++ ex_14_01/dl_14-01.py
@@ -0,0 +1,20 @@
+# Assign a value to x
+x = 'Abc'
+
+# Print out x and the type of x.
+print(x, type(x))
+
+# You will get Abc <class'str'>
+""" 
+> x is the object
+> type() is the method
+> str is the class of variable
+"""
+
+print(dir(x))
+
+"""
+> dir() is the method.
+> dir() will display all the methods you can use with the variable x
+
+"""
